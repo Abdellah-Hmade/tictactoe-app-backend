5a850c3507dc6c404f16eb3205054661608b0316
Abdellah HMADE <abdellah.hmade@uit.ac.ma>
2022-02-11 16:33:18 +0100

    spring boot project for backend api

---
+++ src/main/java/org/hmd377/tictactoeapp/TictactoeAppApplication.java
@@ -0,0 +1,13 @@
+package org.hmd377.tictactoeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hmd377/tictactoeapp/TictactoeAppApplicationTests.java
@@ -0,0 +1,11 @@
+package org.hmd377.tictactoeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeAppApplicationTests {
+
+
+
+}
